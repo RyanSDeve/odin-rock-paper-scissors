599bddbd3eb2b961f55f06e8b0d0f7ad50a4a6d7
Ryan Soares <ryansoaresgithub@gmail.com>
2026-04-11 20:02:09 +0000

    add: create ui

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const resultadoDiv = document.querySelector("#resultado");
+let humanResult = 0;
+let computerResult = 0;
+
 function playRound(humanChoice, computerChoice) {
     let winner = "";
-    if (humanChoice == "pedra" && computerChoice == "tesoura" || humanChoice == "tesoura" && computerChoice == "papel" || humanChoice == "papel" && computerChoice == "pedra"){
+    if (humanChoice == "pedra" && computerChoice == "tesoura" || humanChoice == "tesoura" && computerChoice == "papel" || humanChoice == "papel" && computerChoice == "pedra") {
         humanScore++
         return winner = "Human wins";
-    } else if(humanChoice == computerChoice){
+    } else if (humanChoice == computerChoice) {
         return winner = "Draw"
     } else {
         computerScore++
@@ -37,9 +41,7 @@ function getComputerChoice() {
     return computerChoice.toLowerCase();
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Escolha entre pedra, papel ou tesoura");
-
+function getHumanChoice(humanChoice) {
     return humanChoice.toLowerCase();
 }
 
@@ -47,6 +49,7 @@ function playGame () {
     let draws = 0;
     let humanScore = 0;
     let computerScore = 0;
+    /* 
     for(let rounds = 0; rounds < 5; rounds++){
         let humanChoice = getHumanChoice();
         let computerChoice = getComputerChoice();
@@ -60,24 +63,38 @@ function playGame () {
             draws++
         }
     }    
-
+    */
     console.log(`Esse foi o resultado: Você ${humanScore} X Máquina ${computerScore}`);
+}
 
 
-    function playRound(humanChoice, computerChoice) {
+function playRound(playerSelection) {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice(playerSelection);
     let winner = "";
-    if (humanChoice == "pedra" && computerChoice == "tesoura" || humanChoice == "tesoura" && computerChoice == "papel" || humanChoice == "papel" && computerChoice == "pedra"){
-        alert("Parabéns, você venceu a máquina!!!")
+
+    if(humanResult === 5 || computerResult === 5){
+        const resultadoFinal = document.createElement('h1');
+        resultadoFinal.textContent = `Resultado final: ${humanResult} Humano x ${computerResult} Computador`
+        return resultadoDiv.appendChild(resultadoFinal);
+    }
+
+    if (humanChoice == "pedra" && computerChoice == "tesoura" || humanChoice == "tesoura" && computerChoice == "papel" || humanChoice == "papel" && computerChoice == "pedra") {
+        const novoResultado = document.createElement('p');
+        novoResultado.textContent = `Você escolheu ${humanChoice} e o computador esculheu ${computerChoice}  |  VOCÈ VENCEU!!!`
+        resultadoDiv.appendChild(novoResultado);
+        humanResult++;
         return winner = "Human wins";
-    } else if(humanChoice == computerChoice){
-        alert("Deu empate!")
+    } else if (humanChoice == computerChoice) {
+        const novoResultado = document.createElement('p');
+        novoResultado.textContent = `Você escolheu ${humanChoice} e o computador esculheu ${computerChoice}  |  DEU EMPATE.`
+        resultadoDiv.appendChild(novoResultado);
         return winner = "Draw"
     } else {
-        alert("Computador venceu essa...")
+        const novoResultado = document.createElement('p');
+        novoResultado.textContent = `Você escolheu ${humanChoice} e o computador esculheu ${computerChoice}  |  O COMPUTADOR VENCEU :(`
+        resultadoDiv.appendChild(novoResultado);
+        computerResult++;
         return winner = "Computer wins";
     }
 }
-
-}
-
-playGame();
